df1b1be59310a1b5d89458ba6fcbff39f0d76bb4
Pat <patrick.h.needham@gmail.com>
2014-07-22 08:05:22 -0400

    cleaned up pins scaffold

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,5 +47,4 @@ class PinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
-    end
 end
